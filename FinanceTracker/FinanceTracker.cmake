7a20b08eb588b52c72c4600940465b2f02798d50
Amer <amusinbego1@etf.unsa.ba>
2025-05-11 15:43:45 +0200

    initial refactor

--- FinanceTracker/FinanceTracker.cmake
+++ FinanceTracker/FinanceTracker.cmake
@@ -14,19 +14,4 @@ file(GLOB FINANCE_TRACKER_XML ${MY_INC}/xml/*.h)
 add_executable(${FINANCE_TRACKER_NAME} ${FINANCE_TRACKER_SOURCES} ${FINANCE_TRACKER_INCS} ${FINANCE_TRACKER_COMMON_INCS} ${FINANCE_TRACKER_TD}
 										${FINANCE_TRACKER_CNT} ${FINANCE_TRACKER_MU} ${FINANCE_TRACKER_FO} ${FINANCE_TRACKER_DP} ${FINANCE_TRACKER_XML}) 
 
-#formatirati foldere u IDE.u
-source_group("inc"            		FILES ${FINANCE_TRACKER_INCS})
-source_group("inc\\common"  		FILES ${FINANCE_TRACKER_COMMON_INCS})
-
-source_group("inc\\td"            	FILES ${FINANCE_TRACKER_TD})
-source_group("inc\\cnt"            	FILES ${FINANCE_TRACKER_CNT})
-source_group("inc\\mu"            	FILES ${FINANCE_TRACKER_MU})
-source_group("inc\\fo"            	FILES ${FINANCE_TRACKER_FO})
-source_group("inc\\dp"            	FILES ${FINANCE_TRACKER_DP})
-source_group("inc\\xml"            	FILES ${FINANCE_TRACKER_XML})
-
-source_group("src"   				FILES ${FINANCE_TRACKER_SOURCES})
-
-#target_link_libraries(${FINANCE_TRACKER_NAME} ${MU_LIB_DEBUG})
-
 target_link_libraries(${FINANCE_TRACKER_NAME} debug ${MU_LIB_DEBUG} optimized ${MU_LIB_RELEASE})
--- FinanceTracker/src/LokalniExeHeader.h
+++
@@ -1,2 +0,0 @@
-#pragma once
-//Ovaj header sluzi samo za demonstraciju prikaza lokalnog header fajla (EXE) u IDE-u
