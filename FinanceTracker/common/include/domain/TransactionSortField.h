8cc00b957703710ef819b02951a8e4bf66bfd179
Amer <amusinbego1@etf.unsa.ba>
2025-05-17 12:46:50 +0200

    add TransactionSortField

---
+++ FinanceTracker/common/include/domain/TransactionSortField.h
@@ -0,0 +1,21 @@
+//
+// Created by Amer on 17. 5. 2025..
+//
+
+#ifndef TRANSACTIONSORTFIELD_H
+#define TRANSACTIONSORTFIELD_H
+
+enum class TransactionSortField {
+    DateDesc,
+    DateAsc,
+    CategoryNameAsc,
+    CategoryNameDesc,
+    CategoryTypeAsc,
+    CategoryTypeDesc,
+    AmountDesc,
+    AmountAsc,
+    // Add more as needed
+};
+
+
+#endif //TRANSACTIONSORTFIELD_H
