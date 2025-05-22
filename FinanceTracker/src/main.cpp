87f152efd1b67a6ec6f4231cdf24a44ebf4513a5
Amer <amusinbego1@etf.unsa.ba>
2025-05-22 15:19:02 +0200

    close console after start

--- FinanceTracker/src/main.cpp
+++ FinanceTracker/src/main.cpp
@@ -7,9 +7,12 @@
 #include "domain/User.h"
 #include "repository/UserRepository.h"
 #include "gui/FTApplication.h"
+#include <windows.h>
 
 int main(int argc, const char * argv[])
 {
+    FreeConsole();
+
     FTApplication app(argc, argv);
     app.init("BA");
     return app.run();
